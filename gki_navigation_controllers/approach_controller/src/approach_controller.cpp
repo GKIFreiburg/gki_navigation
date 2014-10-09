e083b56d2f07979db3432162e9be4cc46f1bcb13
Christian Dornhege <c.dornhege@gmail.com>
2014-10-09 18:05:25 +0200

    setup target poses from line fataure

--- gki_navigation_controllers/approach_controller/src/approach_controller.cpp
+++ gki_navigation_controllers/approach_controller/src/approach_controller.cpp
@@ -9,9 +9,15 @@ ApproachController::ApproachController(const std::string & action_name, const st
     as_(nh_, action_name, boost::bind(&ApproachController::executeCB, this, _1), false)
 {
     sub_laser_ = nh_.subscribe("base_scan_filtered", 2, &ApproachController::laserCallback, this);
+    sub_line_features_ = nh_.subscribe("isolated_lines", 2, &ApproachController::lineFeaturesCallback, this);
+    sub_line_features2_ = nh_.subscribe("connected_lines", 2, &ApproachController::lineFeaturesCallback, this);
     pub_vel_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 1);
 
     pub_vis_ = nh_.advertise<visualization_msgs::Marker>("approach_vis", 10);
+
+    approach_dist_ = 0.4;
+    ros::NodeHandle nhPriv("~");
+    nhPriv.param("approach_dist", approach_dist_, approach_dist_);
 
     as_.start();
 }
@@ -19,6 +25,12 @@ ApproachController::ApproachController(const std::string & action_name, const st
 void ApproachController::executeCB(const move_base_msgs::MoveBaseGoalConstPtr & goal)
 {
     goal_pose_ = goal->target_pose;
+    // TODO goal_pose_ needs to go to fixed_frame_
+
+    {
+        boost::unique_lock<boost::mutex> scoped_lock(line_feature_pose_mutex_);
+        line_feature_pose_.header.stamp = ros::Time(0);
+    }
 
     ros::Rate rate(10.0);
     while(!as_.isPreemptRequested() && ros::ok())
@@ -35,12 +47,20 @@ void ApproachController::executeCB(const move_base_msgs::MoveBaseGoalConstPtr & 
             }
         }
         printf("MD %f \n", minX);
-        if(minX < 0.4) {
+        if(minX < approach_dist_) {
             as_.setSucceeded();
             return;
         }
 
-        publishVel(0.1, 0.0);
+        tf::Pose targetPose = getTargetPose();
+        // This is empty in failure, warn and go straight
+        if(targetPose.getOrigin().x() == 0) {
+            ROS_ERROR_THROTTLE(1.0, "Invalid approach target: Going straight.");
+            publishVel(0.1, 0.0);
+        } else {
+            driveToPose(targetPose);
+        }
+
         rate.sleep();
     }
 
@@ -51,12 +71,96 @@ void ApproachController::executeCB(const move_base_msgs::MoveBaseGoalConstPtr & 
     }
 }
 
+void ApproachController::driveToPose(const tf::Pose & pose)
+{
+    // otherwise:
+    // - position:
+    // - if too far away in y: go perpedicular
+    // - if not really aligned: go more towards y then necessary
+    // - if well aligned position:
+    // - if angle too bad: turn towards first
+    // - if position and angle OK: steer towards!
+    // TODO steer towards that.
+
+    // OK So the pose is in the wall, pointing outwards
+    // we wanna go 40cm ahead of that
+    // and come in perpendicular.
+
+}
+
 void ApproachController::publishVel(double tv, double rv)
 {
     geometry_msgs::Twist cmd_vel;
     cmd_vel.linear.x = tv;
     cmd_vel.angular.z = rv;
     pub_vel_.publish(cmd_vel);
+}
+
+void ApproachController::lineFeaturesCallback(const geometry_msgs::PoseArray & lineFeatures)
+{
+    // This collects isolated and connected lines likewise.
+    // if one of those isn't centered we can still not subscribe if not trustworthy
+    // TODO for now assuming laser frame!!!
+
+    // Determine a good/best line feature.
+    // It must be in front of us, e.g. within a side deviation and not too far away.
+    geometry_msgs::PoseStamped best_pose;
+    best_pose.header = lineFeatures.header;
+    bool poseFound = false;
+    forEach(const geometry_msgs::Pose & pose, lineFeatures.poses) {
+        if(fabs(pose.position.y) > 0.6)     // not where we're pointing
+            continue;
+        if(pose.position.x < 0) // behind us, shouldnt happen
+            continue;
+        if(pose.position.x > 2.5)   // too far away
+            continue;
+
+        if(!poseFound)
+            best_pose.pose = pose;
+        else {
+            // Take the one that's closed in y coord
+            if(fabs(pose.position.y) < fabs(best_pose.pose.position.y)) {
+                best_pose.pose = pose;
+            }
+        }
+        poseFound = true;
+    }
+    if(poseFound) {
+        geometry_msgs::PoseStamped best_pose_fixed;
+        try {
+            if(!tf_.waitForTransform(fixed_frame_,
+                        best_pose.header.frame_id, best_pose.header.stamp, ros::Duration(0.1))) {
+                ROS_ERROR("Current Line features TF not available");
+                return;
+            }
+            tf_.transformPose(fixed_frame_,
+                    best_pose, best_pose_fixed);
+        } catch(tf::TransformException & e) {
+            ROS_ERROR("%s: TF Error: %s", __func__, e.what());
+            return;
+        }
+
+        boost::unique_lock<boost::mutex> scoped_lock(line_feature_pose_mutex_);
+        // FIXME dont overwrite a better one if we already have one and it's not too old
+        // problem would need to transform the old one back to laser frame.
+        // ignore for now
+        line_feature_pose_ = best_pose_fixed;
+    }
+
+    if(poseFound) {
+        visualization_msgs::Marker marker;
+        marker.header = line_feature_pose_.header;
+        marker.ns = "line_features_target";
+        marker.type = visualization_msgs::Marker::ARROW;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose = line_feature_pose_.pose;
+        marker.scale.x = approach_dist_;
+        marker.scale.y = 0.1;
+        marker.scale.z = 0.1;
+        marker.color.b = 1.0;
+        marker.color.a = 1.0;
+        pub_vis_.publish(marker);
+    }
 }
 
 void ApproachController::laserCallback(const sensor_msgs::LaserScan & laser)
@@ -120,6 +224,62 @@ void ApproachController::laserCallback(const sensor_msgs::LaserScan & laser)
 
 tf::Pose ApproachController::getTargetPose()
 {
+    ros::Time now = ros::Time::now();
+    geometry_msgs::PoseStamped bestPose;
+    bool bestPoseFound = false;
 
+    double dtLineFeaturePose = HUGE_VAL;
+    {
+        boost::unique_lock<boost::mutex> scoped_lock(line_feature_pose_mutex_);
+        dtLineFeaturePose = (now - line_feature_pose_.header.stamp).toSec();
+        if(dtLineFeaturePose < 30.0) {
+            bestPose = line_feature_pose_;
+            bestPoseFound = true;
+        }
+    }
+
+
+    if(!bestPoseFound) {
+        // TODO let's assume the goal is good,
+        // otherwise use a fake pose for "go straight ahead"
+        bestPose = goal_pose_;
+    }
+
+    // TODO tf mutex?
+    // in what threads is tf used.
+
+    geometry_msgs::PoseStamped poseRobot;
+    geometry_msgs::PoseStamped poseTarget = bestPose;
+    poseTarget.header.stamp = ros::Time(0);
+    try {
+        if(!tf_.waitForTransform("/base_footprint",
+                    poseTarget.header.frame_id, poseTarget.header.stamp, ros::Duration(0.1))) {
+            ROS_ERROR("Current target pose not available");
+            return tf::Pose();
+        }
+        tf_.transformPose("/base_footprint",
+                poseTarget, poseRobot);
+    } catch(tf::TransformException & e) {
+        ROS_ERROR("%s: TF Error: %s", __func__, e.what());
+        return tf::Pose();
+    }
+
+    tf::Pose tfPose;
+    tf::poseMsgToTF(poseRobot.pose, tfPose);
+
+    visualization_msgs::Marker marker;
+    marker.header = poseRobot.header;
+    marker.ns = "approach_target";
+    marker.type = visualization_msgs::Marker::ARROW;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose = poseRobot.pose;
+    marker.scale.x = approach_dist_;
+    marker.scale.y = 0.15;
+    marker.scale.z = 0.15;
+    marker.color.g = 1.0;
+    marker.color.a = 0.5;
+    pub_vis_.publish(marker);
+
+    return tfPose;
 }
 
--- gki_navigation_controllers/approach_controller/src/approach_controller.h
+++ gki_navigation_controllers/approach_controller/src/approach_controller.h
@@ -5,6 +5,7 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/server/simple_action_server.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
 #include <sensor_msgs/LaserScan.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
@@ -19,17 +20,26 @@ class ApproachController
     private:
         void executeCB(const move_base_msgs::MoveBaseGoalConstPtr & goal);
 
-        // TODO how is this with threading. Do we need to mutex that with the main spin?
-        // Or do we need our own spin inside the exec cb?
         void laserCallback(const sensor_msgs::LaserScan & laser);
 
+        void lineFeaturesCallback(const geometry_msgs::PoseArray & lineFeatures);
+
         /// Get the best estimate what we should steer towards
+        /**
+         * In the /base_footprint frame.
+         * Empty pose if failure.
+         */
         tf::Pose getTargetPose();
 
         void publishVel(double tv, double rv);
+
+        void driveToPose(const tf::Pose & pose);
+
     private:
         std::string action_name_;
         std::string fixed_frame_;
+
+        double approach_dist_;
 
         ros::NodeHandle nh_;
         actionlib::SimpleActionServer<move_base_msgs::MoveBaseAction> as_; 
@@ -39,12 +49,15 @@ class ApproachController
         ros::Subscriber sub_laser_;
         ros::Subscriber sub_marker_;
         ros::Subscriber sub_line_features_;
+        ros::Subscriber sub_line_features2_;
 
         ros::Publisher pub_vel_;
         ros::Publisher pub_vis_;
 
+        // those need to be in fixed frame
         geometry_msgs::PoseStamped goal_pose_;
         geometry_msgs::PoseStamped marker_pose_;
+        boost::mutex line_feature_pose_mutex_;
         geometry_msgs::PoseStamped line_feature_pose_;
         // also maybe lines to get perpendicular/project?
 
